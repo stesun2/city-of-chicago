ca3ae6efabd88fe4274b2439af8296abf13134ab
stesun2 <ss27@illinois.edu>
2021-06-24 15:36:54 -0500

    started queries

--- chicago_queries.sql
+++ chicago_queries.sql
@@ -1,1 +1,11 @@
 -- Write your Queries here
+
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees(
+  first_name            VARCHAR(24) NOT NULL,
+  last_name             VARCHAR(24) NOT NULL,
+  job_title             VARCHAR(24) NOT NULL,
+  full_or_part_time     CHAR(1) NOT NULL,
+  department            VARCHAR(24) NOT NULL,
+  annual_salary         INTEGER NOT NULL
+);
